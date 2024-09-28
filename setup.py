2a109cdf2af8b188a63f5b6b7d3a3db0b96903c6
Rishi Raj Kataria <151474794+r-kataria@users.noreply.github.com>
2024-09-28 02:28:20 +0100

    chore: bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="probabilistic.py",
-    version="0.1.0",
+    version="0.1.1",
     author="R Kataria",
     description="A package for probabilistic function execution.",
     long_description=open('README.md').read(),
